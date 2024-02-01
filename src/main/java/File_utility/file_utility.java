b7718b98d173bd15908ec9acdd67f6b84985bcff
Mayank <ms34116722@gmail.com>
2024-02-01 14:31:30 +0530

    fileutility

--- src/main/java/File_utility/file_utility.java
+++ src/main/java/File_utility/file_utility.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class file_utility {
 
 	public String fetchfromthepropertyfile(String key) throws IOException {
@@ -16,4 +19,11 @@ public class file_utility {
 		return pro.getProperty(key);
 
 	}
+	
+	public static void Selectclass (WebElement element,int value) {
+		Select select= new Select(element);
+		select.selectByIndex(value);
+	}
+	
+	
 }
